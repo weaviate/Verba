aca722b5596d75fea3e346536b4a3b390498ccba
thomashacker <EdwardSchmuhl@web.de>
2023-08-28 16:32:43 -0700

    Setup cli commands

--- verba/server/cli_start.py
+++ verba/server/cli_start.py
@@ -1,5 +1,8 @@
 import click
 import uvicorn
+from verba.ingestion.cli import init as init_ingest
+from verba.ingestion.cli import import_data_command as import_data
+from verba.ingestion.cli import import_weaviate_command as import_weaviate
 
 
 @click.group()
@@ -15,6 +18,10 @@ def start():
     """
     uvicorn.run("verba.server.api:app", host="0.0.0.0", port=8000, reload=True)
 
+
+cli.add_command(init_ingest, name="init")
+cli.add_command(import_data, name="import")
+cli.add_command(import_weaviate, name="weaviate")
 
 if __name__ == "__main__":
     cli()
