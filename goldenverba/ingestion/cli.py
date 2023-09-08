b0a87058c5f4beafef1806c153f77e644a3dcf4b
Duda Nogueira <dudanogueira@gmail.com>
2023-09-08 20:40:10 -0300

    check more connections, auth and errors. init_schema should return true in order to proceed with other steps

--- goldenverba/ingestion/cli.py
+++ goldenverba/ingestion/cli.py
@@ -1,4 +1,5 @@
 import click
+from wasabi import msg  # type: ignore[import]
 from goldenverba.ingestion.init_schema import init_schema
 from goldenverba.ingestion.init_cache import init_cache
 from goldenverba.ingestion.init_suggestion import init_suggestion
@@ -22,10 +23,11 @@ def init(model):
     """
     Initialize schemas
     """
-    init_schema(model=model)
-    init_cache()
-    init_suggestion()
-
+    if(init_schema(model=model)):
+        init_cache()
+        init_suggestion()
+    else:
+        msg.fail("Could not init schema")
 
 @cli.command()
 def clear_cache_command():
@@ -39,9 +41,11 @@ def clear_cache_command():
     help="OpenAI Model name to initialize. (default gpt-3.5-turbo)",
 )
 def clear_all_command(model):
-    init_schema(model=model)
-    init_cache()
-    init_suggestion()
+    if(init_schema(model=model)):
+        init_cache()
+        init_suggestion()
+    else:
+        msg.fail("Could not init schema")
 
 
 @cli.command()
@@ -62,15 +66,17 @@ def clear_all_command(model):
 )
 def import_data_command(path, model, clear):
     if clear:
-        init_schema(model=model)
-        init_cache()
-        init_suggestion()
+        if(init_schema(model=model)):
+            init_cache()
+            init_suggestion()
+        else:
+            msg.fail("Could not init schema")
     import_data(path, model)
 
 
 @cli.command()
 def import_weaviate_command():
-    init_schema(model="gpt-4")
-    init_cache()
-    init_suggestion()
-    import_weaviate()
+    if init_schema(model="gpt-4"):
+        init_cache()
+        init_suggestion()
+        import_weaviate()
