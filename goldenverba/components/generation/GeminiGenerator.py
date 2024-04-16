53f1d8b0ad06a836b7007fa99ea7ff1535e276fc
ajit283 <55892788+ajit283@users.noreply.github.com>
2024-04-16 13:36:34 +0200

    update gemini generator

--- goldenverba/components/generation/GeminiGenerator.py
+++ goldenverba/components/generation/GeminiGenerator.py
@@ -1,8 +1,10 @@
 import asyncio
 import os
 from collections.abc import Iterator
-import vertexai
-from vertexai.preview.generative_models import GenerativeModel
+import vertexai.preview
+from vertexai.preview.generative_models import GenerativeModel, Content, Part
+from wasabi import msg
+
 from dotenv import load_dotenv
 
 from goldenverba.components.generation.interface import Generator
@@ -10,7 +12,7 @@ from goldenverba.components.generation.interface import Generator
 load_dotenv()
 
 
-class GPT4Generator(Generator):
+class GeminiGenerator(Generator):
     """
     Gemini Generator.
     """
@@ -20,10 +22,14 @@ class GPT4Generator(Generator):
         self.name = "GeminiGenerator"
         self.description = "Generator using Google's Gemini 1.5 Pro model"
         self.requires_library = ["google-cloud-aiplatform"]
-        self.requires_env = ["GOOGLE_APPLICATION_CREDENTIALS", "GOOGLE_CLOUD_PROJECT"]
+        self.requires_env = [
+            "GOOGLE_APPLICATION_CREDENTIALS",
+            "GOOGLE_CLOUD_PROJECT",
+        ]
         self.streamable = True
         self.model_name = os.getenv("GEMINI_MODEL", "gemini-1.5-pro-preview-0409")
         self.context_window = 10000
+        self.system_instruction = "You are Verba, The Golden RAGtriever, a chatbot for Retrieval Augmented Generation (RAG). You will receive a user query and context pieces that have a semantic similarity to that specific query. Please answer these user queries only their provided context. If the provided documentation does not provide enough information, say so. If the user asks questions about you as a chatbot specifially, answer them naturally. If the answer requires code examples encapsulate them with ```programming-language-name ```. Don't do pseudo-code."
 
     async def generate(
         self,
@@ -48,18 +54,16 @@ class GPT4Generator(Generator):
             REGION = "us-central1"
             vertexai.init(project=project_id, location=REGION)
 
-            chat_completion_arguments = {"model": self.model_name, "messages": messages}
-            if openai.api_type == "azure":
-                chat_completion_arguments["deployment_id"] = self.model_name
-
-            base_url = os.environ.get("OPENAI_BASE_URL", "")
-            if base_url:
-                openai.api_base = base_url
+            generative_multimodal_model = GenerativeModel(
+                "gemini-1.5-pro-preview-0409",
+                system_instruction=self.system_instruction,
+            )
+            # response = generative_multimodal_model.generate_content(messages)
 
             completion = await asyncio.to_thread(
-                openai.ChatCompletion.create, **chat_completion_arguments
+                generative_multimodal_model.generate_content, **messages
             )
-            system_msg = str(completion["choices"][0]["message"]["content"])
+            system_msg = str(completion.candidates[0].content.parts[0].text)
 
         except Exception:
             raise
@@ -83,48 +87,43 @@ class GPT4Generator(Generator):
         messages = self.prepare_messages(queries, context, conversation)
 
         try:
-            import openai
 
-            openai.api_key = os.getenv("OPENAI_API_KEY")
-            base_url = os.environ.get("OPENAI_BASE_URL", "")
-            if base_url:
-                openai.api_base = base_url
+            project_id = os.getenv("GOOGLE_CLOUD_PROJECT")
 
-            if "OPENAI_API_TYPE" in os.environ:
-                openai.api_type = os.getenv("OPENAI_API_TYPE")
-            if "OPENAI_API_BASE" in os.environ:
-                openai.api_base = os.getenv("OPENAI_API_BASE")
-            if "OPENAI_API_VERSION" in os.environ:
-                openai.api_version = os.getenv("OPENAI_API_VERSION")
+            REGION = "us-central1"
+            vertexai.init(project=project_id, location=REGION)
 
-            chat_completion_arguments = {
-                "model": self.model_name,
-                "messages": messages,
-                "stream": True,
-                "temperature": 0.0,
-            }
-            if openai.api_type == "azure":
-                chat_completion_arguments["deployment_id"] = self.model_name
-
-            completion = await openai.ChatCompletion.acreate(
-                **chat_completion_arguments
+            generative_multimodal_model = GenerativeModel(
+                "gemini-1.5-pro-preview-0409",
+                system_instruction=self.system_instruction,
             )
+            # response = generative_multimodal_model.generate_content(messages)
+
+            completion = await generative_multimodal_model.generate_content_async(
+                stream=True, contents=messages
+            )
+
+            print("using google gemini")
+
+            iter = completion.__aiter__()
 
             try:
                 while True:
-                    chunk = await completion.__anext__()
-                    if len(chunk["choices"]) > 0:
-                        if "content" in chunk["choices"][0]["delta"]:
+                    chunk = await iter.__anext__()
+                    if len(chunk.candidates) > 0:
+                        msg.info(chunk.candidates[0])
+                        if len(chunk.candidates[0].content.parts[0].text) > 0:
                             yield {
-                                "message": chunk["choices"][0]["delta"]["content"],
-                                "finish_reason": chunk["choices"][0]["finish_reason"],
+                                "message": chunk.candidates[0].content.parts[0].text,
+                                "finish_reason": chunk.candidates[0].finish_reason,
                             }
                         else:
                             yield {
                                 "message": "",
-                                "finish_reason": chunk["choices"][0]["finish_reason"],
+                                "finish_reason": chunk.candidates[0].finish_reason,
                             }
             except StopAsyncIteration:
+                msg.info("DONE")
                 pass
 
         except Exception:
@@ -132,7 +131,7 @@ class GPT4Generator(Generator):
 
     def prepare_messages(
         self, queries: list[str], context: list[str], conversation: dict[str, str]
-    ) -> dict[str, str]:
+    ) -> list[Content]:
         """
         Prepares a list of messages formatted for a Retrieval Augmented Generation chatbot system, including system instructions, previous conversation, and a new user query with context.
 
@@ -144,24 +143,50 @@ class GPT4Generator(Generator):
 
         Each message in the list is a dictionary with 'role' and 'content' keys, where 'role' is either 'system' or 'user', and 'content' contains the relevant text. This will depend on the LLM used.
         """
-        messages = [
-            {
-                "role": "system",
-                "content": "You are Verba, The Golden RAGtriever, a chatbot for Retrieval Augmented Generation (RAG). You will receive a user query and context pieces that have a semantic similarity to that specific query. Please answer these user queries only their provided context. If the provided documentation does not provide enough information, say so. If the user asks questions about you as a chatbot specifially, answer them naturally. If the answer requires code examples encapsulate them with ```programming-language-name ```. Don't do pseudo-code.",
-            }
-        ]
+        messages = []
 
         for message in conversation:
-            messages.append({"role": message.type, "content": message.content})
+            messages.append(
+                Content(role=message.type, parts=[Part.from_text(message.content)])
+            )
 
         query = " ".join(queries)
         user_context = " ".join(context)
 
         messages.append(
-            {
-                "role": "user",
-                "content": f"Please answer this query: '{query}' with this provided context: {user_context}",
-            }
+            Content(
+                role="user",
+                parts=[
+                    Part.from_text(
+                        f"Please answer this query: '{query}' with this provided context: {user_context}"
+                    )
+                ],
+            )
         )
+        
+        messages = self.ensure_user_model_alteration(messages)
+        
+        msg.info(f"Generated Messages: {[message.role for message in messages]}")
 
         return messages
+    
+    def ensure_user_model_alteration(self, messages: list[Content]) -> list[Content]:
+        
+        current_role: str = ""
+        
+        new_messages: list[Content] = []
+        
+
+        
+        for message in messages:
+            if message.role == current_role:
+                new_messages[-1] = message
+            else:
+                new_messages.append(message)
+                current_role = message.role
+        
+        return new_messages
+                
+            
+        
+        
