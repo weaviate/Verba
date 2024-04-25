29b9b6df77a0c5571a19535f2891d8e7c8aebbf1
thomashacker <EdwardSchmuhl@web.de>
2024-04-25 09:43:01 +0200

    Fix schema and environment support

--- goldenverba/components/schema/schema_generation.py
+++ goldenverba/components/schema/schema_generation.py
@@ -27,17 +27,27 @@ def verify_vectorizer(
         skip_properties = []
     modified_schema = schema.copy()
 
-    #adding specific config for Azure OpenAI
+    # adding specific config for Azure OpenAI
     vectorizer_config = None
-    if os.getenv("OPENAI_API_TYPE") == "azure" and vectorizer=="text2vec-openai":
+    if os.getenv("OPENAI_API_TYPE") == "azure" and vectorizer == "text2vec-openai":
         resourceName = os.getenv("AZURE_OPENAI_RESOURCE_NAME")
         model = os.getenv("AZURE_OPENAI_EMBEDDING_MODEL")
         if resourceName is None or model is None:
-            raise Exception("AZURE_OPENAI_RESOURCE_NAME and AZURE_OPENAI_EMBEDDING_MODEL should be set when OPENAI_API_TYPE is azure. Resource name is XXX in http://XXX.openai.azure.com")
-        vectorizer_config = { 
-            "text2vec-openai": {
-                    "deploymentId": model,
-                    "resourceName": resourceName
+            raise Exception(
+                "AZURE_OPENAI_RESOURCE_NAME and AZURE_OPENAI_EMBEDDING_MODEL should be set when OPENAI_API_TYPE is azure. Resource name is XXX in http://XXX.openai.azure.com"
+            )
+        vectorizer_config = {
+            "text2vec-openai": {"deploymentId": model, "resourceName": resourceName}
+        }
+
+    # adding specific config for Google
+    if vectorizer == "text2vec-palm":
+        project = os.getenv("GOOGLE_CLOUD_PROJECT")
+        if project is None:
+            raise Exception("GOOGLE_CLOUD_PROJECT is not set")
+        vectorizer_config = {
+            "text2vec-palm": {
+                "projectId": project,
             }
         }
 
